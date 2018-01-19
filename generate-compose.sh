d7db5a65b3767993e91592e8cf6d645beb131683
root <root@bedc.k.time4vps.cloud>
2018-01-19 21:43:55 +0200

    Add php configuration

--- generate-compose.sh
+++ generate-compose.sh
@@ -1,46 +1,8 @@
 #! /bin/bash
 
 cat > docker-compose.yml <<:EOF:
-
 version: '2'
 services:
-    varnish:
-        ports:
-            - 80:80
-        image: million12/varnish
-        environment:
-            - VCL_CONFIG=/data/generated.vcl
-        volumes:
-            - $PWD/varnish/vcl:/data
-        links:
-:EOF:
-for site in `ls sites`
-do
-    SITENAME="`echo $site|tr -d .`"
-cat >> docker-compose.yml <<:EOF:
-            - $SITENAME
-    $SITENAME:
-        ports:
-            - 8080:8080
-        image: nginx:1.13.6
-        links:
-            - mariadb
-            - php-$SITENAME:php
-        volumes:
-            - $PWD/sites/$site:/var/www/$site
-            - $PWD/nginx-base-conf:/etc/nginx/
-            - $PWD/nginx/non-ssl:/etc/nginx/conf.d
-    php-$SITENAME:
-        image: php:7-fpm
-        volumes:
-            - $PWD/sites/$site:/var/www/$site
-            - $PWD/nginx/php-fpm.conf:/usr/local/etc/php-fpm.conf
-        environment:
-            - TIMEZONE=Europe/Helsinki
-:EOF:
-
-done
-cat >> docker-compose.yml << :EOF:
     ssl-terminate:
         image: nginx:1.13.6
         ports:
@@ -57,3 +19,48 @@ cat >> docker-compose.yml << :EOF:
         env_file:
             - $PWD/pw/mysql
 :EOF:
+
+for site in `ls sites`
+do
+    SITENAME="`echo $site|tr -d .`"
+cat >> docker-compose.yml <<:EOF:
+    $SITENAME:
+        ports:
+            - 8080:8080
+        image: nginx:1.13.6
+        links:
+            - mariadb
+            - php-$SITENAME:php
+        volumes:
+            - $PWD/sites/$site:/var/www/$site
+            - $PWD/nginx-base-conf:/etc/nginx/
+            - $PWD/nginx/non-ssl:/etc/nginx/conf.d
+    php-$SITENAME:
+        image: php:7-fpm
+        volumes:
+            - $PWD/sites/$site:/var/www/$site
+	    - $PWD/nginx/php-fpm.conf:/usr/local/etc/php-fpm.d/logging.conf
+            - $PWD/nginx/php.ini:/usr/local/etc/php/php.ini
+        environment:
+            - TIMEZONE=Europe/Helsinki
+:EOF:
+done
+cat >> docker-compose.yml << :EOF:
+    varnish:
+        ports:
+            - 80:80
+        image: million12/varnish
+        environment:
+            - VCL_CONFIG=/data/generated.vcl
+        volumes:
+            - $PWD/varnish/vcl:/data
+        links:
+:EOF:
+for site in `ls sites`
+do
+    SITENAME="`echo $site|tr -d .`"
+cat >> docker-compose.yml <<:EOF:
+            - $SITENAME
+:EOF:
+
+done
